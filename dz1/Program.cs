cf8fc0021062a9b9bb75da223317333c2fc3919e
Tatiana <tt_tanya_88@mail.ru>
2022-10-15 13:58:01 +0300

    dz1

---
+++ dz1/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задайте массив заполненный случайными положительными трёхзначными 
+// числами. Напишите программу, которая покажет количество чётных чисел в массиве.
+
+Console.WriteLine("Введите размер массива: ");
+int size = Convert.ToInt32(Console.ReadLine());
+
+int[] array = new int[size];
+Random myRandom = new Random();
+ 
+Console.WriteLine("Наш массив: ");
+for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = myRandom.Next(100, 999);
+        Console.Write("{0} ", array[i]);
+    }
+
+int count = 0;
+
+for (int i = 0; i < array.Length; i++)
+if (array[i] % 2 == 0)
+count++;
+
+Console.WriteLine();
+Console.WriteLine($"Из них четных чисел: {count}");
